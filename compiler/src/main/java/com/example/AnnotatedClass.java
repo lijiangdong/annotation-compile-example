c9c82383e6726b5ff84b7c573fea0fd9c359d4e9
lijiangdong <lijiangdong@plu.cn>
2016-11-01 21:24:11 +0800

    commit

--- compiler/src/main/java/com/example/AnnotatedClass.java
+++ compiler/src/main/java/com/example/AnnotatedClass.java
@@ -1,36 +1,29 @@
 package com.example;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
 import java.util.List;
 
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
 
 /**
  * Created by lijiangdong on 2016/10/31.
  */
 public class AnnotatedClass {
-    private TypeElement annotatedClassElement;
-    private String qualifiedSuperClassName;
-    private String simpleTypeName;
+    private TypeElement typeElement;
     private List<ContentType> contentTypes;
     private List<InjectViewField> injectViewFields;
-
-    public AnnotatedClass(TypeElement annotatedClassElement) {
-        this.annotatedClassElement = annotatedClassElement;
-        qualifiedSuperClassName = annotatedClassElement.getQualifiedName().toString();
-        simpleTypeName = annotatedClassElement.getSimpleName().toString();
-    }
-
-
-    public TypeElement getTypeElement() {
-        return annotatedClassElement;
+    private Elements elementUtils;
+    public AnnotatedClass(TypeElement typeElement, Elements elementUtils) {
+        this.typeElement = typeElement;
+        this.elementUtils = elementUtils;
     }
 
     public String getQualifiedSuperClassName() {
-        return qualifiedSuperClassName;
-    }
-
-    public String getSimpleTypeName() {
-        return simpleTypeName;
+        return typeElement.getQualifiedName().toString();
     }
 
     public void addInjectView(InjectViewField injectViewField){
@@ -39,5 +32,16 @@ public class AnnotatedClass {
 
     public void addContentType(ContentType contentType){
         contentTypes.add(contentType);
+    }
+
+    public JavaFile generateJavaFile(){
+        MethodSpec methodSpec = MethodSpec.methodBuilder("")
+                .build();
+        TypeSpec typeSpec = TypeSpec.classBuilder("")
+                .build();
+        return JavaFile
+                .builder(elementUtils.getPackageOf(typeElement).getQualifiedName().toString(),typeSpec)
+                .build();
+
     }
 }
